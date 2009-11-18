84e4b4697c270abe72f4bd2ee9b99f01ccee2097
yygao <>
2009-11-18 21:16:41 +0000

    Add plugins module

---
+++ LhcTrackAnalyzer/plugins/SealModules.cc
@@ -0,0 +1,6 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "UserCode/LhcTrackAnalyzer/interface/LhcTrackAnalyzer.h"
+
+// DEFINE_SEAL_MODULE();
+DEFINE_FWK_MODULE(LhcTrackAnalyzer);
