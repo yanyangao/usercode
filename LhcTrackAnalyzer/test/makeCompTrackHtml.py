73950a85d1a594cb0d5e9d0a07b284a72e697cea
yygao <>
2010-05-07 12:12:22 +0000

    change plots

--- LhcTrackAnalyzer/test/makeCompTrackHtml.py
+++ LhcTrackAnalyzer/test/makeCompTrackHtml.py
@@ -40,8 +40,10 @@ if not os.path.isdir(indir) :
     print "Cannot find %s.  Exiting." % infile
     sys.exit()
 
-if os.path.isfile(indir+"index.html") :
-    print "%s already exists.  Exiting." % indir+"index.html"
+if os.path.isfile(indir+"trackingcomp.html") :
+    cpindexfile = "cp trackingcomp.html %sindex.html" % (indir)
+    os.system(cpindexfile)
+    
 
 files = glob.glob(indir+'*png')
 
@@ -51,29 +53,3 @@ for file in files:
     
     command = "convert %s.png -resize 250x250 %s_small.png" % (base, base)
     os.system(command)
-
-
-os.system("rm -f "+indir+"index.html")
-os.system("touch "+indir+"index.html")
-findex = open(indir+"index.html","w")
-
-print >> findex, "<h1> "+title+"</h1>"
-print >> findex, "<hr>\n"
-print >> findex, "<table>"
-
-nfile = 0
-for file in files:
-    base = os.path.basename(file).split(".")[0]
-    print base
-
-    if nfile == 0: print >> findex, "<tr>"
-    elif nfile % 3 == 0: print >> findex, "</tr>\n<tr>"
-    
-    print >> findex, '<td><a href="%s.png"><img src="%s_small.png"></a></td>' % (base, base)  
-    nfile += 1
-
-print >> findex, "</tr>\n</table>\n"
-
-
-
-findex.close()     
