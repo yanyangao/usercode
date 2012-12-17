8ae0941925c09570f00c9327717e65c61987c3c2
jaehyeok <>
2012-12-17 16:33:52 +0000

    get mlfit.root as well

--- HWW/scripts/SubmitToys/gcRunNormTest.sh
+++ HWW/scripts/SubmitToys/gcRunNormTest.sh
@@ -17,7 +17,7 @@ ANA=$4
 NJETS=$5
 
 
-OUTPUTDIR=/afs/cern.ch/user/y/yygao/scratch0/ana_PostHCP_2D_19fb/
+OUTPUTDIR=/afs/cern.ch/user/j/jaehyeok/CMSSW_5_2_3_patch4/src/Smurf/LimitCalc/ana_PostHCP_2D_19fb/
 mkdir -p $OUTPUTDIR/logsNorm/
 
 
@@ -42,7 +42,7 @@ INJ=\$1
 MIN=\$2
 MAX=\$3
 MASS=\$4
-cd /tmp/yygao/
+cd /tmp/jaehyeok/
 scramv1 p CMSSW CMSSW_5_3_3
 cd CMSSW_5_3_3/src
 eval \`scramv1 ru -sh\`
@@ -51,25 +51,27 @@ scramv1 b
 perl -p -i -e "s/exp\_/exp\_final\_/g" HiggsAnalysis/CombinedLimit/test/mlfitNormsToText.py
 cp $WORKDIR/getPseudoData.C .
 root -b -q getPseudoData.C\(\${MASS},\"8TeV\",\"shape\",$NJETS,\"of\",\"${ANA}\",\${MIN},\${MAX}\)
-echo 'processing mass='\${MASS} analysis ${ANA} $NJET jet>> /tmp/yygao/CMSSW_5_3_3/src/log_\${INJ}_\${MASS}_\${MIN}_\${MAX}_$ANA_${NJETS}j_summary.log
+echo 'processing mass='\${MASS} analysis ${ANA} $NJET jet>> /tmp/jaehyeok/CMSSW_5_3_3/src/log_\${INJ}_\${MASS}_\${MIN}_\${MAX}_$ANA_${NJETS}j_summary.log
 for NEXP in \$(seq \${MIN} \${MAX})
   do
   echo 'toy='\${NEXP} 
   DIRPATH=testcards/cards_\${INJ}_N\${NEXP}/\${MASS}/
   cd \${DIRPATH}
   combine -M MaxLikelihoodFit --saveNorm ${ANA}of_${NJETS}j_shape_8TeV.txt -m \${MASS} #>& /dev/null
-  python ../../../HiggsAnalysis/CombinedLimit/test/mlfitNormsToText.py mlfit.root >> /tmp/yygao/CMSSW_5_3_3/src/logNorm_\${INJ}_\${MASS}_${ANA}of_${NJETS}j_shape_8TeV_\${MIN}_\${MAX}.log
-  python ../../../HiggsAnalysis/CombinedLimit/test/diffNuisances.py -a mlfit.root >> /tmp/yygao/CMSSW_5_3_3/src/logNuis_\${INJ}_\${MASS}_${ANA}of_${NJETS}j_shape_8TeV_\${MIN}_\${MAX}.log
+  python ../../../HiggsAnalysis/CombinedLimit/test/mlfitNormsToText.py mlfit.root >> /tmp/jaehyeok/CMSSW_5_3_3/src/logNorm_\${INJ}_\${MASS}_${ANA}of_${NJETS}j_shape_8TeV_\${MIN}_\${MAX}.log
+  python ../../../HiggsAnalysis/CombinedLimit/test/diffNuisances.py -a mlfit.root >> /tmp/jaehyeok/CMSSW_5_3_3/src/logNuis_\${INJ}_\${MASS}_${ANA}of_${NJETS}j_shape_8TeV_\${MIN}_\${MAX}.log
+  cp mlfit.root /tmp/jaehyeok/CMSSW_5_3_3/src/mlfit_injm${INJ}_m\${MASS}_${NJETS}j_id\${NEXP}.root 
   rm mlfit.root higgsCombineTest.MaxLikelihoodFit.mH125.root
-  cd /tmp/yygao/CMSSW_5_3_3/src
+  cd /tmp/jaehyeok/CMSSW_5_3_3/src
 done
 mkdir -p $OUTPUTDIR/logsNorm/\${MASS}/
 # clear up old files
 rm -f $OUTPUTDIR/logsNorm/\${MASS}/logNorm_\${INJ}_\${MASS}_${ANA}of_${NJETS}j_shape_8TeV_\${MIN}_\${MAX}.log
 rm -f $OUTPUTDIR/logsNorm/\${MASS}/logNuis_\${INJ}_\${MASS}_${ANA}of_${NJETS}j_shape_8TeV_\${MIN}_\${MAX}.log
 rm -f $OUTPUTDIR/logsNorm/\${MASS}/log_\${INJ}_\${MASS}_\${MIN}_\${MAX}_$ANA_${NJETS}j_summary.log
-cp /tmp/yygao/CMSSW_5_3_3/src/log* $OUTPUTDIR/logsNorm/\${MASS}/
-cd /tmp/yygao/
+cp /tmp/jaehyeok/CMSSW_5_3_3/src/log* $OUTPUTDIR/logsNorm/\${MASS}/
+cp /tmp/jaehyeok/CMSSW_5_3_3/src/mlfit_* $OUTPUTDIR/logsNorm/\${MASS}/
+cd /tmp/jaehyeok/
 rm -rf CMSSW_5_3_3
 EOF
 
