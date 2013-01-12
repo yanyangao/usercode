577b209a19ea8ac3117cb0bc45b8a5fe6e6937df
yygao <>
2013-01-12 15:10:13 +0000

    default create 10000 toys

--- HWW/scripts/SubmitToys/createtoydata.sh
+++ HWW/scripts/SubmitToys/createtoydata.sh
@@ -12,4 +12,4 @@ if [ ! $# -eq 1 ]; then
 fi
 
 
-$LANDS -d $CARDNAME.txt  -M Hybrid  -m 125 --minuitSTRATEGY 0 --bWriteToys 1 -n $CARDNAME --nToysForCLsb 1000 --nToysForCLb 1 --singlePoint 1 --seed 12344  -rMin 0 -rMax 5
+$LANDS -d $CARDNAME.txt  -M Hybrid  -m 125 --minuitSTRATEGY 0 --bWriteToys 1 -n $CARDNAME --nToysForCLsb 10000 --nToysForCLb 1 --singlePoint 1 --seed 12344  -rMin 0 -rMax 5
