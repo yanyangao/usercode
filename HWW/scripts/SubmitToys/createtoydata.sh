a934dd705a234223c0bf0214d1522ba10d17a801
yygao <>
2013-01-23 12:53:34 +0000

    generate all toys

--- HWW/scripts/SubmitToys/createtoydata.sh
+++ HWW/scripts/SubmitToys/createtoydata.sh
@@ -3,13 +3,8 @@
 # make sure the lands directory is correct
 
 LANDS="../../../../LandS/test/lands.exe"
-CARDNAME=$1
-
-if [ ! $# -eq 1 ]; then
-    echo " USAGE: ./createtoydata.sh cardname
-    cardname - name of the card, e.g.hwwof_0j_shape_8TeV, ! without .txt !"
-    exit 1
-fi
 
 
-$LANDS -d $CARDNAME.txt  -M Hybrid  -m 125 --minuitSTRATEGY 0 --bWriteToys 1 -n $CARDNAME --nToysForCLsb 10000 --nToysForCLb 1 --singlePoint 1 --seed 12344  -rMin 0 -rMax 5
+for CARDNAME in hwwof_0j_shape_8TeV  hwwof_1j_shape_8TeV xwwof_0j_shape_8TeV  xwwof_1j_shape_8TeV; do 
+    $LANDS -d $CARDNAME.txt  -M Hybrid  -m 125 --minuitSTRATEGY 0 --bWriteToys 1 -n $CARDNAME --nToysForCLsb 10000 --nToysForCLb 1 --singlePoint 1 --seed 12344  -rMin 0 -rMax 5
+done
